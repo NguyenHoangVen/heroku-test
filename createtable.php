ed82d3b7de184dfd44bf912ee4844fdfe27e432c
Oai <dinhoai23061998@gmail.com>
2020-05-12 03:50:49 +0000

    PosgreSql create table

--- createtable.php
+++ createtable.php
@@ -6,25 +6,25 @@ function pg_connection_string_from_database_url(){
 	$db = pg_connect(pg_connection_string_from_database_url());
 	if(!$db) {
 		echo"Error:unable to open database \n";
-}else{
+	}else{
 	echo "Opnened database successfully \n"
-}
+	}
 
-	$sql=<<<EOF
+	$sql="
 		CREATE TABLE COMPANY2
 		(ID INT FRIMARY KEY NOT NULL,
 		NAME		TEXT NOT NULL,
 		AGE		INT NOT NULL,
 		ADDRESS		CHAR(50),
 		SALARY		REAL);
-EOF;
+"
 
 	$ret = pg_query($db,$sql);
 	if(!ret){
 		echo pg_last_error($db);
-}else{
+	}else{
 		echo "table created successfully \n"
-}
+	}
 	pg_close($db);
 
  ?>
