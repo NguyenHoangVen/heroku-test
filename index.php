92dca927a0f8e8cc904da4c4ec55f84a68606ecb
Oai <dinhoai23061998@gmail.com>
2020-05-12 07:32:10 +0000

    add bai 9

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
+<h1>Chu de 6</h1>
+<Form name="myform" action="index.php" method="POST">
+<input type="hidden" name="my_tag" value=" "/>
+Ho ten: <input type="text" name="name" value=""/>
+Email: <input type="email" name="email" value=""/>
+<input type="submit" value="Submit"/>
+</Form>
 
-<html>
-<head>
-<title>Oai dep trai</title>
-</head>
-<body>
-<form action="home.php" method="get">
-<input type="text" name="txtname" placeholder="please input a name"><br>
-<input type ="text" name="txtemail" placeholder="please input an Email "><br>
-<input type="submit" value="submit    xcnv,d">
-</form>
-</body>
-</html>
+<?php
+echo $_POST['my_tag'];
+echo "<br></br>";
+$name = "";
+$name = $_POST['name'];
+if($name != ""){
+	print_r("Chao ban " . $_POST['name'] . ", email cua ban la" . $_POST['email']);
+}
+?>
+
+<br><a href=setup.php>Create Database</a>
+<br><a href=add_account.php>Add Account</a>
+<br><a href=list_account.php>List Account</a>
