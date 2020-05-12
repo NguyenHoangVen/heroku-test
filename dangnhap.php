521cc1866b67a92f80c8a4461fb6c516630695c1
Oai <dinhoai23061998@gmail.com>
2020-05-12 02:09:28 +0000

    first commit my app

---
+++ dangnhap.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Oai dep trai</title>
+</head>
+<body>
+<form action="home.php" method="get">
+<input type="text" name="txtname" placeholder="please input a name"><br>
+<input type ="text" name="txtemail" placeholder="please input an Email "><br>
+<input type="submit" value="submit">
+</form>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+Welcome <?php echo $_GET["txtname"] ?><br>
+Your email address is :<?php echo $_GET["txtemail"]; ?>
+</body>
+</html>
